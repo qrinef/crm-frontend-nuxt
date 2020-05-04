f595b4a2a13e81296fc1ac91c5fe4bcbe867f503
qrinef <qrinef@gmail.com>
2020-05-04 09:34:04 +0300

    fix: users loading

--- pages/users.vue
+++ pages/users.vue
@@ -1,6 +1,6 @@
 <template>
   <el-main>
-    <el-table v-loading="isLoading" v-infinite-scroll="setItems" :data="items">
+    <el-table v-loading="isLoading" :data="items">
       <el-table-column label="ID" prop="id" width="100" />
       <el-table-column label="Name" prop="name" />
       <el-table-column label="Surname" prop="surname" />
@@ -10,14 +10,8 @@
   </el-main>
 </template>
 
-<style>
-  .el-main {
-    background: #f3f3f3;
-  }
-</style>
-
 <script>
-import { mapState, mapActions, mapMutations } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'Users',
@@ -25,14 +19,13 @@ export default {
     ...mapState('users', ['isLoading', 'items'])
   },
   watch: {
-    $route: 'resetState'
+    $route: 'setItems'
   },
   created () {
-    this.resetState()
+    this.setItems()
   },
   methods: {
-    ...mapActions('users', ['setItems']),
-    ...mapMutations('users', ['resetState'])
+    ...mapActions('users', ['setItems'])
   }
 }
 </script>
