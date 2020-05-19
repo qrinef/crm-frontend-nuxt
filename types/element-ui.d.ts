f13fd711d2d889a6aadbe70c3a8744c1a0cb1085
qrinef <qrinef@gmail.com>
2020-05-20 01:24:21 +0300

    fix: element ui scrollbar declaration

--- types/element-ui.d.ts
+++ types/element-ui.d.ts
@@ -1,1 +1,20 @@
-declare module 'element-ui'
+import Vue from 'vue'
+
+declare class ElementUIComponent extends Vue {
+  static install (vue: typeof Vue): void
+}
+
+declare class ElScrollbar extends ElementUIComponent {
+  native: boolean
+  wrapStyle: any
+  wrapClass: any
+  viewClass: any
+  viewStyle: any
+  noresize: boolean
+  tag: string
+}
+
+declare module 'element-ui/types' {
+  interface Scrollbar extends ElScrollbar {}
+  export class Scrollbar extends ElScrollbar {}
+}
